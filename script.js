da877f46e52dadbb539aeccea3034f1aa90ebdf9
Irina <iram4@yandex.ru>
2022-01-27 17:55:32 +0300

    Выполнение первого домашнего задания

---
+++ script.js
@@ -0,0 +1,9 @@
+let title,
+    screens,
+    screenPrice,
+    rollback,
+    fullPrice,
+    adaptive;
+
+alert("Да здравствует JS!) Ну, с богом! Поехали..");
+console.log("Проверка связи с консолью. Все ок!");
